f66268cb408ba136ff76d39711d633407d84342f
simiyutin <simiyutin.boris@yandex.ru>
2016-12-25 20:40:46 +0300

    ++

--- src/test/first_class_functions/src/com/company/Main.java
+++ src/test/first_class_functions/src/com/company/Main.java
@@ -7,39 +7,8 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args)  throws Exception  {
-        ArrayList<Invoice> allInvoices= new ArrayList<>();
-
-        allInvoices.add(new Invoice());
-
-        List<Invoice> test2 = findInvoicesFromMicrosoft(allInvoices);
-        List<Invoice> test3 = findInvoicesFromOracle(allInvoices);
-    }
-
-    public static List<Invoice> findInvoicesFromOracle(List<Invoice> invoices) throws Exception {
-        List<Invoice> result = new ArrayList<>();
-        for(Invoice invoice: invoices) {
-            if(invoice.getCustomer() == Customer.ORACLE) {
-                result.add(invoice);
-            }
-        }
-        return result;
-    }
-
-    public static  List<Invoice> findInvoicesFromMicrosoft(List<Invoice> invoices) {
-        List<Invoice> result = new ArrayList<>();
-        for(Invoice invoice: invoices) {
-            if(invoice.getCustomer() == Customer.MICROSOFT) {
-                result.add(invoice);
-            }
-        }
-        return result;
-    }
-}
-
-    /*
     public static void main(String[] args) {
-	ArrayList<Invoice> allInvoices= new ArrayList<>();
+        ArrayList<Invoice> allInvoices= new ArrayList<>();
 
         allInvoices.add(new Invoice());
 
@@ -66,4 +35,4 @@ public class Main {
         }
         return result;
     }
-    */
+}
